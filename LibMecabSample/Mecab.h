956db4e03f33116b41ca9d1f9151baa37cc44ba9
Takuji Matsugaki <gbh01467@nifty.com>
2016-01-14 22:58:27 +0900

    ファーストリリース

--- LibMecabSample/Mecab.h
+++ LibMecabSample/Mecab.h
@@ -5,7 +5,14 @@
 //  Copyright 2010 FLCL.jp. All rights reserved.
 //
 
+// static library などを含むアプリのシッピング用アーカイブ作成手順
+// *****************************************************************************
+// 1.Release タグで Skip Install=YES
+// 2.Copy Headers で mecab のヘッダーを従来の public から project にする。
+// 3.上に付帯してラッパーがオリジナルのパスを見失うのでワールドサーチにして相対パスでアサインする。
+// *****************************************************************************
 #include <mecab.h>
+//#include "../mecab/mecab.h"
 #import <UIKit/UIKit.h>
 
 
